4e4d7e5cb3631a5580d2f4e4828ea373db1c664e
k4r1m63k <karimbek97@gmail.com>
2025-05-08 01:39:01 +0300

    FEAT: add selected

--- src/components/SelectedItemsBox.vue
+++ src/components/SelectedItemsBox.vue
@@ -7,21 +7,28 @@ interface Props {
   products?: Product[];
   product?: Product | null;
   multiple?: boolean;
+  limit: number;
 }
 
 withDefaults(defineProps<Props>(), {
   multiple: false,
+  limit: 6,
 })
 </script>
 
 <template>
-  <div class="box" :class="{'multiple': products.length }" v-if="multiple">
-    <template v-if="products.length">
-      <ProductItem v-for="product in products" :key="product.id" :product="product"/>
-    </template>
+  <div v-if="multiple" class="box__top-wrapper">
+    <div v-if="products.length">
+      <div class="box__top" :class="{'multiple': products.length }">
+        <ProductItem v-for="product in products" :key="product.id" :product="product"/>
+      </div>
+      <div class="box__selected">
+        selected {{products.length}} / {{ limit }}
+      </div>
+    </div>
     <span class="box__empty" v-else>Выберите продукты</span>
   </div>
-  <div class="box" v-else>
+  <div class="box__bottom" v-else>
     <ProductItem v-if="product" :product="product"/>
     <span class="box__empty" v-else>Выберите продукт</span>
   </div>
@@ -29,12 +36,34 @@ withDefaults(defineProps<Props>(), {
 
 <style lang="scss">
 .box {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid black;
-  min-height: 100px;
-  padding: 20px;
+
+  &__selected {
+    margin-top: 20px;
+  }
+
+  &__top {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 10px;
+
+    &-wrapper {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 10px;
+      border: 1px solid black;
+      padding: 20px;
+    }
+  }
+
+  &__bottom {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    min-height: 100px;
+    padding: 20px;
+  }
 
   &.multiple {
     display: grid;
