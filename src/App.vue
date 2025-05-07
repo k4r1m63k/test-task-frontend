2da3688762419103abf1bae3007169eedc7020aa
k4r1m63k <karimbek97@gmail.com>
2025-05-07 16:40:48 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Test task frontend</h1>
+  <main>
+    <RouterView />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div>Hello World</div>
+</template>
